e011fb1f7e13b427509612f39536fef576d49410
sdi2200288 <altanitontou1@.com>
2025-10-09 12:51:05 +0300

    new lsh

--- lsh.cpp
+++ lsh.cpp
@@ -1,10 +1,17 @@
 //LSH ALGORITHM
-
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <random>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
 #include "lsh.hpp"
 
-lsh :: lsh(int seed1, std::string input_file1, std::string query_file1, std::string output_file1, int k1, int L1, float w1, int N1, float R1, std::string type1, bool range1){
+using namespace std;
+lsh :: lsh(int seed1, string input_file1, string query_file1, string output_file1, int k1, int L1, float w1, int N1, float R1, string type1, bool range1){
     seed = seed1;
     input_file = input_file1;
     query_file = query_file1;
@@ -16,10 +23,91 @@ lsh :: lsh(int seed1, std::string input_file1, std::string query_file1, std::str
     R = R1;
     type = type1;
     range = range1;
+
+    dimension = (type == "mnist") ? 784 : 128;
+
+    cout << "LSH Constructed!" << endl;
 }
 
 lsh :: ~lsh(){
-    cout << "Deconstructed!" << endl;
- }
+    cout << "LSH Deconstructed!" << endl;
+}
 
 
+//Αρχικοποίηση τυχαίων διανυσμάτων (v) κ μετατοπίσεων (t)
+void lsh:: Initialize(){
+    cout<<"Initialize v and t"<<endl;
+
+    default_random_engine generator(seed);
+    normal_distribution<double> normal_dist(0.0,1.0);
+    uniform_real_distribution<double> uniform_dist(0.0, w);
+
+    randomvectors.resize(L);
+    randomsifts.resize(L);
+
+    for(int i=0; i<L ; i++){
+        randomvectors[i].resize(k);
+        randomvsifts[i].resize(k);
+        
+        for(int j=0; j<k ; j++){
+            randomvectors[i][j].resize(dimension);  // Δημιουργία τυχαίου διανύσματος v διάστασης 178(SIFT)
+            for(int dim = 0 ; dim < dimension ; dim++){
+                randomvectors[i][j][dim] = normal_dist(generator);
+            }
+            randomvsifts[i][j] = uniform_dist(generator);  // Δημιουργία τυχαίου διανύσματος t διάστασης 178(SIFT)
+        }
+    }
+
+}
+
+uint64_t lsh:: CreateHFun(const vector<double>& point, int tableIndex){
+    int sum =0;
+    
+
+    for (int i=0; i<k; i++){
+        double dot_product =0;
+        for(int dim = 0 ; dim < dimension ; dim++){
+            dot_product += randomvectors[tableIndex][i][dim]*p[i];
+        } 
+
+        if(dot_product > INT_MAX || dot_product < INT_MIN){
+            cout<<"Overflow"<<endl;
+            exit(EXIT_FAILURE);
+        }
+        double numerator = dot_product + randomvsifts[tableIndex][i];
+     
+        return sum = floor( (double) (numerator) / w)
+    }
+}
+
+//Δημιουργία hash tables
+void lsh:: CreateHashTables(){
+    cout<<"Create L:"<< L << "HashTables" <<endl;
+
+    hashTables.resize(L);
+
+    for(int i =0 ; i < L; i++){
+        for(int j =0 ; j < dataset.size(); j++){
+            uint64_t hashV = CreateHFun(dataset[j],i);
+            hashTables[i][j].push_back(j);
+        }    
+    }
+}
+
+void lsh_func(){
+
+    cout<<"LSH Algorithm" <<endl;
+
+    Initialize();
+    CreateHashTables();
+    
+}
+
+void print_params() {
+    std::cout << "input_file: " << input_file << "\n"
+              << "query_file: " << query_file << "\n"
+              << "output_file: " << output_file << "\n"
+              << "k: " << k << ", L: " << L << ", w: " << w << ", N: " << N << ", R: " << R << "\n"
+              << "type: " << type << "\n"
+              << "range: " << boolalpha << range << "\n";
+}
--- lsh.hpp
+++ lsh.hpp
@@ -2,6 +2,11 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <random>
+#include <string>
+#include <functional>
+
 using namespace std;
 
 class lsh{
@@ -11,23 +16,23 @@ class lsh{
         double w, R;
         bool range;
 
-        // std::vector<std::map<uint64_t, std::vector<int>> hashTables;
+        vector<unordered_map<uint64_t, vector<int>>> hashTables; // L hash-tables
+        vector<vector<vector<double>>> randomvectors; // [L][k][dimension]
+        vector<vector<double>> randomshifts;  //Τυχαίες μετατοπίσεις [L][k]
+       
+        int dimension; //178 ή 784
+
+        
 
     public:
-        lsh(int seed1, std::string input_file1, std::string query_file1, std::string output_file1, int k1, int L1, float w1, int N1, float R1, std::string type1, bool range1);
+        lsh(int seed1, string input_file1, string query_file1, string output_file1, int k1, int L1, float w1, int N1, float R1, std::string type1, bool range1);
         ~lsh();
-        void lsh_func(int seed, std::string input_file, std::string query_file, std::string output_file, int k, int L, float w, int N, float R, std::string type, bool range){
+       
+        void lsh_func();
 
-            
-        }
-
-         void print_params() {
-            std::cout << "input_file: " << input_file << "\n"
-                    << "query_file: " << query_file << "\n"
-                    << "output_file: " << output_file << "\n"
-                    << "k: " << k << ", L: " << L << ", w: " << w << ", N: " << N << ", R: " << R << "\n"
-                    << "type: " << type << "\n"
-                    << "range: " << std::boolalpha << range << "\n";
-        }
+        void Initialize();
+        void CreateHashTables();
+        uint64_t CreateHFun(const vector<double>& point, int tableIndex);
+        void print_params();
 };
 
