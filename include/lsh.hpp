a3e536f656e40e74898a5cd94f9c51c3c07babbe
sdi2200288 <altanitontou1@.com>
2025-10-12 17:32:44 +0300

    new lsh.hpp

--- include/lsh.hpp
+++ include/lsh.hpp
@@ -9,6 +9,10 @@
 #include <functional>
 #include <cmath>
 #include <cstdint>
+#include <chrono>  
+#include <algorithm> 
+#include <fstream> 
+#include <unordered_set>
 
 using namespace std;
 
@@ -20,7 +24,6 @@ class lsh{
         int dimension; //128 ή 784
         size_t dataset_n;
 
-        // προσθήκη ως μέλη της κλάσης lsh
         uint64_t M = 4294967291; // prime ~ 2^32-5
         vector<vector<uint64_t>> random_r; // L x k
         size_t tableSize = 0;
@@ -37,13 +40,18 @@ class lsh{
         
         //void lsh_func(int seed, std::string input_file, std::string query_file, std::string output_file, int k, int L, float w, int N, float R, std::string type, bool range);
    
-        void lsh_func(const vector<vector<double>>& dataset);
+        void lsh_func(const vector<vector<double>>& dataset,const vector<vector<double>>& queries);
 
         void Initialize(size_t dataset_n);
         void CreateHashTables(const vector<vector<double>>& dataset);
 
         int CreateHFun(const vector<double>& point, int tableIndex);
         void print_params();
+        void Queries(const vector<vector<double>>& queries,const vector<vector<double>>& dataset);
+        double euclidean_distance(const vector<double>& v1, const vector<double>& v2);
+        vector<pair<int, double>> ANN(const vector<double>& query,const vector<vector<double>>& dataset,int N);
+        //vector<pair<int, double>> ENN(const vector<double>& query, const vector<vector<double>>& dataset, int N);
+        //vector<pair<int, double>> RangeSearch(const vector<double>& query, const vector<vector<double>>& dataset, int N);
 };
 
 #endif
